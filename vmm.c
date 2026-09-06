84aec486a2c5a3f67edeb9b54e361acb2b6494e0
sina hosseinizad <24656395+wo-von@users.noreply.github.com>
2026-09-06 16:44:12 +0200

    first draft, builds and runs clean

--- vmm.c
+++ vmm.c
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <err.h>
 #include <linux/kvm.h>
+#include <sys/mman.h>
+#include <string.h>
 
 const uint8_t code[] = {
     0xba, 0xf8, 0x03, /* mov $0x3f8, %dx */
@@ -32,4 +34,81 @@ int main() {
         err(1, "KVM_GET_API_VERSION expexted 12 got %d\n", ret);
     }
     
+    ret = ioctl(kvm, KVM_CHECK_EXTENSION, KVM_CAP_USER_MEMORY);
+    if (ret == -1) {
+        err(1, "KVM_CHECK_EXTENSION");
+    }
+    if (!ret){
+        err(1, " required extension KVM_CAP_USER_MEMORY fails");
+    }
+    
+    int vmfd = ioctl(kvm, KVM_CREATE_VM, (unsigned long) 0);
+    
+    void* mem = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    if (mem == MAP_FAILED){
+        err(1, "mmap failed");
+    }
+    memcpy(mem, code, sizeof(code));
+
+    struct kvm_userspace_memory_region region = {
+        .slot = 0,
+        .guest_phys_addr = 0x1000,
+        .memory_size = 0x1000,
+        .userspace_addr = (uint64_t)mem,
+    };
+    
+    ret = ioctl(vmfd, KVM_SET_USER_MEMORY_REGION, &region);
+    if (ret == -1){
+        err(1, "KVM_USER_MEMORY_REGION ioctl failed");
+    }
+    
+    int vcpufd = ioctl(vmfd, KVM_CREATE_VCPU, (unsigned long) 0);
+    if (vcpufd == -1){
+        err(1, "VCPU KVM_VCPU_CREATE failed");
+    }
+    
+    int mmap_size = ioctl(kvm, KVM_GET_VCPU_MMAP_SIZE, NULL); // to communicate with the userspace
+    struct kvm_run *run = (struct kvm_run *)mmap(NULL, mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED, vcpufd, 0);
+    // read the sregs and set cs to 0
+    struct kvm_sregs  sregs;
+    ioctl(vcpufd, KVM_GET_SREGS, &sregs);
+    sregs.cs.base = 0;
+    sregs.cs.selector = 0;
+    ioctl(vcpufd, KVM_SET_SREGS, &sregs);
+    struct kvm_regs regs = {
+        .rip = 0x1000,
+        .rax = 2,
+        .rbx = 2,
+        .rflags = 0x2,
+    };
+    ioctl(vcpufd, KVM_SET_REGS, &regs);
+    while(1){
+        ioctl(vcpufd, KVM_RUN, NULL);
+        switch (run->exit_reason){
+            case KVM_EXIT_HLT:
+                puts("KVM_EXIT_HLT");
+                return 0;
+        
+                break;    
+            case KVM_EXIT_IO:
+                if (run->io.direction == KVM_EXIT_IO_OUT &&
+                        run->io.size == 1 &&
+                        run->io.port == 0x3f8 &&
+                        run->io.count == 1)
+                    putchar(*(((char *)run + run->io.data_offset)));
+                else
+                    errx(1, "unhandled KVM_EXIT_IO");
+                break;
+            case KVM_EXIT_FAIL_ENTRY:
+                errx(1, 
+                    "KVM_EXIT_FAIL_ENTRY: hardware_entry_failure_reason = 0x%llx", 
+                    (unsigned long long) run->fail_entry.hardware_entry_failure_reason);
+                break;        
+            case KVM_EXIT_INTERNAL_ERROR:
+                    errx(1, "KVM_EXIT_INTERNAL_ERROR: suberror = 0x%x",
+                        run->internal.suberror);
+        }
+
+    }
 }
