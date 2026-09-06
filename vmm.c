12a15fd844ec57628008167b2773a7540821fc6c
sina hosseinizad <24656395+wo-von@users.noreply.github.com>
2026-09-06 16:51:14 +0200

    format

--- vmm.c
+++ vmm.c
@@ -1,52 +1,52 @@
+#include <err.h>
 #include <fcntl.h>
+#include <linux/kvm.h>
 #include <stdint.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <err.h>
-#include <linux/kvm.h>
-#include <sys/mman.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 const uint8_t code[] = {
     0xba, 0xf8, 0x03, /* mov $0x3f8, %dx */
-	0x00, 0xd8,       /* add %bl, %al */
-	0x04, '0',        /* add $'0', %al */
-	0xee,             /* out %al, (%dx) */
-	0xb0, '\n',       /* mov $'\n', %al */
-	0xee,             /* out %al, (%dx) */
-	0xf4,             /* hlt */
+    0x00, 0xd8,       /* add %bl, %al */
+    0x04, '0',        /* add $'0', %al */
+    0xee,             /* out %al, (%dx) */
+    0xb0, '\n',       /* mov $'\n', %al */
+    0xee,             /* out %al, (%dx) */
+    0xf4,             /* hlt */
 };
 
-int kvm; 
+int kvm;
 int main() {
     kvm = open("/dev/kvm", O_RDWR | O_CLOEXEC);
-    if (kvm == -1){
-        err(1, "cannot open /dev/kvm");        
+    if (kvm == -1) {
+        err(1, "cannot open /dev/kvm");
     }
 
     int ret = ioctl(kvm, KVM_GET_API_VERSION, NULL);
     if (ret == -1) {
         err(1, "KVM_GET_API_VERSION ");
     }
-    if (ret != 12){
+    if (ret != 12) {
         err(1, "KVM_GET_API_VERSION expexted 12 got %d\n", ret);
     }
-    
+
     ret = ioctl(kvm, KVM_CHECK_EXTENSION, KVM_CAP_USER_MEMORY);
     if (ret == -1) {
         err(1, "KVM_CHECK_EXTENSION");
     }
-    if (!ret){
+    if (!ret) {
         err(1, " required extension KVM_CAP_USER_MEMORY fails");
     }
-    
-    int vmfd = ioctl(kvm, KVM_CREATE_VM, (unsigned long) 0);
-    
-    void* mem = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
-    if (mem == MAP_FAILED){
+    int vmfd = ioctl(kvm, KVM_CREATE_VM, (unsigned long) 0);
+
+    void *mem = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    if (mem == MAP_FAILED) {
         err(1, "mmap failed");
     }
     memcpy(mem, code, sizeof(code));
@@ -55,23 +55,24 @@ int main() {
         .slot = 0,
         .guest_phys_addr = 0x1000,
         .memory_size = 0x1000,
-        .userspace_addr = (uint64_t)mem,
+        .userspace_addr = (uint64_t) mem,
     };
-    
+
     ret = ioctl(vmfd, KVM_SET_USER_MEMORY_REGION, &region);
-    if (ret == -1){
+    if (ret == -1) {
         err(1, "KVM_USER_MEMORY_REGION ioctl failed");
     }
-    
+
     int vcpufd = ioctl(vmfd, KVM_CREATE_VCPU, (unsigned long) 0);
-    if (vcpufd == -1){
+    if (vcpufd == -1) {
         err(1, "VCPU KVM_VCPU_CREATE failed");
     }
-    
+
     int mmap_size = ioctl(kvm, KVM_GET_VCPU_MMAP_SIZE, NULL); // to communicate with the userspace
-    struct kvm_run *run = (struct kvm_run *)mmap(NULL, mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED, vcpufd, 0);
+    struct kvm_run *run =
+        (struct kvm_run *) mmap(NULL, mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED, vcpufd, 0);
     // read the sregs and set cs to 0
-    struct kvm_sregs  sregs;
+    struct kvm_sregs sregs;
     ioctl(vcpufd, KVM_GET_SREGS, &sregs);
     sregs.cs.base = 0;
     sregs.cs.selector = 0;
@@ -83,32 +84,27 @@ int main() {
         .rflags = 0x2,
     };
     ioctl(vcpufd, KVM_SET_REGS, &regs);
-    while(1){
+    while (1) {
         ioctl(vcpufd, KVM_RUN, NULL);
-        switch (run->exit_reason){
-            case KVM_EXIT_HLT:
-                puts("KVM_EXIT_HLT");
-                return 0;
-        
-                break;    
-            case KVM_EXIT_IO:
-                if (run->io.direction == KVM_EXIT_IO_OUT &&
-                        run->io.size == 1 &&
-                        run->io.port == 0x3f8 &&
-                        run->io.count == 1)
-                    putchar(*(((char *)run + run->io.data_offset)));
-                else
-                    errx(1, "unhandled KVM_EXIT_IO");
-                break;
-            case KVM_EXIT_FAIL_ENTRY:
-                errx(1, 
-                    "KVM_EXIT_FAIL_ENTRY: hardware_entry_failure_reason = 0x%llx", 
-                    (unsigned long long) run->fail_entry.hardware_entry_failure_reason);
-                break;        
-            case KVM_EXIT_INTERNAL_ERROR:
-                    errx(1, "KVM_EXIT_INTERNAL_ERROR: suberror = 0x%x",
-                        run->internal.suberror);
-        }
+        switch (run->exit_reason) {
+        case KVM_EXIT_HLT:
+            puts("KVM_EXIT_HLT");
+            return 0;
 
+            break;
+        case KVM_EXIT_IO:
+            if (run->io.direction == KVM_EXIT_IO_OUT && run->io.size == 1 &&
+                run->io.port == 0x3f8 && run->io.count == 1)
+                putchar(*(((char *) run + run->io.data_offset)));
+            else
+                errx(1, "unhandled KVM_EXIT_IO");
+            break;
+        case KVM_EXIT_FAIL_ENTRY:
+            errx(1, "KVM_EXIT_FAIL_ENTRY: hardware_entry_failure_reason = 0x%llx",
+                 (unsigned long long) run->fail_entry.hardware_entry_failure_reason);
+            break;
+        case KVM_EXIT_INTERNAL_ERROR:
+            errx(1, "KVM_EXIT_INTERNAL_ERROR: suberror = 0x%x", run->internal.suberror);
+        }
     }
 }
