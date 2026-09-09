9e2c7ec57232a81693ed915834f8115052878c4f
sina hosseinizad <24656395+wo-von@users.noreply.github.com>
2026-09-09 18:22:25 +0200

    debugging for guest page table

--- vmm.c
+++ vmm.c
@@ -29,7 +29,67 @@ const uint8_t code[] = {
     0xf4,             /* hlt */
 };
 
-static void dump_state(int vcpufd) {
+#define PDE64_PRESENT (1U << 0)
+#define PDE64_RW (1U << 1)
+#define PDE64_USER (1U << 2)
+#define PDE64_PS (1U << 7)
+
+/* Walks the guest's page tables from the host side. `mem` is the host
+ * pointer backing guest-physical memory; `cr3` is the guest-physical
+ * address of the PML4, read out of the vcpu's sregs. Assumes an identity
+ * mapping between guest-physical addresses and offsets into `mem`, which
+ * is what this VMM's memory layout uses. */
+static void dump_page_tables(void *mem, uint64_t cr3) {
+    uint64_t *pml4 = (uint64_t *) ((uint8_t *) mem + cr3);
+    for (int i = 0; i < 512; i++) {
+        if (!(pml4[i] & PDE64_PRESENT))
+            continue;
+        printf("PML4[%d] = %llX (RW=%d US=%d) -> PDPT @ %llX\n", i, (unsigned long long) pml4[i],
+               !!(pml4[i] & PDE64_RW), !!(pml4[i] & PDE64_USER),
+               (unsigned long long) (pml4[i] & ~0xFFFULL));
+
+        uint64_t *pdpt = (uint64_t *) ((uint8_t *) mem + (pml4[i] & ~0xFFFULL));
+        for (int j = 0; j < 512; j++) {
+            if (!(pdpt[j] & PDE64_PRESENT))
+                continue;
+            if (pdpt[j] & PDE64_PS) {
+                printf("  PDPT[%d] = %llX (RW=%d US=%d PS=1) -> 1GiB page @ %llX\n", j,
+                       (unsigned long long) pdpt[j], !!(pdpt[j] & PDE64_RW),
+                       !!(pdpt[j] & PDE64_USER), (unsigned long long) (pdpt[j] & ~0xFFFULL));
+                continue;
+            }
+            printf("  PDPT[%d] = %llX (RW=%d US=%d) -> PD @ %llX\n", j,
+                   (unsigned long long) pdpt[j], !!(pdpt[j] & PDE64_RW), !!(pdpt[j] & PDE64_USER),
+                   (unsigned long long) (pdpt[j] & ~0xFFFULL));
+
+            uint64_t *pd = (uint64_t *) ((uint8_t *) mem + (pdpt[j] & ~0xFFFULL));
+            for (int k = 0; k < 512; k++) {
+                if (!(pd[k] & PDE64_PRESENT))
+                    continue;
+                if (pd[k] & PDE64_PS) {
+                    printf("    PD[%d] = %llX (RW=%d US=%d PS=1) -> 2MiB page @ %llX\n", k,
+                           (unsigned long long) pd[k], !!(pd[k] & PDE64_RW), !!(pd[k] & PDE64_USER),
+                           (unsigned long long) (pd[k] & ~0xFFFULL));
+                    continue;
+                }
+                printf("    PD[%d] = %llX (RW=%d US=%d) -> PT @ %llX\n", k,
+                       (unsigned long long) pd[k], !!(pd[k] & PDE64_RW), !!(pd[k] & PDE64_USER),
+                       (unsigned long long) (pd[k] & ~0xFFFULL));
+
+                uint64_t *pt = (uint64_t *) ((uint8_t *) mem + (pd[k] & ~0xFFFULL));
+                for (int l = 0; l < 512; l++) {
+                    if (!(pt[l] & PDE64_PRESENT))
+                        continue;
+                    printf("      PT[%d] = %llX (RW=%d US=%d) -> 4KiB page @ %llX\n", l,
+                           (unsigned long long) pt[l], !!(pt[l] & PDE64_RW), !!(pt[l] & PDE64_USER),
+                           (unsigned long long) (pt[l] & ~0xFFFULL));
+                }
+            }
+        }
+    }
+}
+
+static void dump_state(int vcpufd, void *mem) {
     struct kvm_regs reg;
     struct kvm_sregs sreg;
     int ret = ioctl(vcpufd, KVM_GET_REGS, &reg);
@@ -50,6 +110,7 @@ static void dump_state(int vcpufd) {
            sreg.cs.l, sreg.cs.db);
     printf("ss base=%llX limit=%X type=%X l=%X db=%X\n", sreg.ss.base, sreg.ss.limit, sreg.ss.type,
            sreg.ss.l, sreg.ss.db);
+    dump_page_tables(mem, sreg.cr3);
 }
 int main() {
     int kvm = open("/dev/kvm", O_RDWR | O_CLOEXEC);
@@ -137,7 +198,7 @@ int main() {
     while (1) {
         ret = ioctl(vcpufd, KVM_RUN, NULL);
         if (ret == -1) {
-            dump_state(vcpufd);
+            dump_state(vcpufd, mem);
             err(1, "KVM_RUN_FAILED");
         }
         switch (run->exit_reason) {
@@ -154,16 +215,16 @@ int main() {
                 errx(1, "unhandled KVM_EXIT_IO");
             break;
         case KVM_EXIT_FAIL_ENTRY:
-            dump_state(vcpufd);
+            dump_state(vcpufd, mem);
             errx(1, "KVM_EXIT_FAIL_ENTRY: hardware_entry_failure_reason = 0x%llx",
                  (unsigned long long) run->fail_entry.hardware_entry_failure_reason);
             break;
         case KVM_EXIT_INTERNAL_ERROR:
-            dump_state(vcpufd);
+            dump_state(vcpufd, mem);
             errx(1, "KVM_EXIT_INTERNAL_ERROR: suberror = 0x%x", run->internal.suberror);
             break;
         default:
-            dump_state(vcpufd);
+            dump_state(vcpufd, mem);
             errx(1, "check KVM exit");
         }
     }
